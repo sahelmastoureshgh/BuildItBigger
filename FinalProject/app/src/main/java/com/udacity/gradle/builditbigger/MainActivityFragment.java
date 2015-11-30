1aa4aba3e452f1ce2e29047814a5001718271a81
Sahel Mastoureshgh <sahelmastoureshgh@Sahels-iMac.local>
2015-11-29 17:43:12 -0800

    Added java library and its dependency

--- FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -1,13 +1,15 @@
 package com.udacity.gradle.builditbigger;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.mastoureshgh.JokeTeller;
 
 
 /**
@@ -31,6 +33,10 @@ public class MainActivityFragment extends Fragment {
                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
                 .build();
         mAdView.loadAd(adRequest);
+        TextView textView = (TextView) root.findViewById(R.id.instructions_text_view);
+        JokeTeller jt=new JokeTeller();
+        textView.setText(jt.getJoke());
+
         return root;
     }
 }
